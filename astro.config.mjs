c8d4594faa107c09421569e287da5584cf8d3ac0
SEBASTIAN <97497900+Sebastian-IngSoftware@users.noreply.github.com>
2025-09-21 22:28:03 -0500

    Fix site URL casing in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Sebastian-IngSoftware.github.io'
+  site: 'https://sebastian-ingsoftware.github.io',
 });
